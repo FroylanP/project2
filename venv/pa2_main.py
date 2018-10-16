b6f508f0b2cc7259bc4bee9610b1f034e384e17a
Froylan Perez <p.froylan@yahoo.com>
2018-10-15 20:44:05 -0600

    project 2

---
+++ venv/pa2_main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    def cap_join():
+
+        mylist = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
+        mylist.append("calvin")
+        mylist.append("and")
+        mylist.append("hobbes")
+        mylist.append("are")
+        mylist.append("the")
+        mylist.append("first")
+        mylist.append("spaceman")
+        mylist.append("on")
+        mylist.append('mars')
+
+        print("".join(mylist))
+
